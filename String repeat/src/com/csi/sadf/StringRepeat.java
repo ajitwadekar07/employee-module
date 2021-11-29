c82529b4110b17eca6527be54ba425e851fbd15f
ajitwadekar07 <88771063+ajitwadekar07@users.noreply.github.com>
2021-11-29 12:35:28 +0530

    sasas

---
+++ String repeat/src/com/csi/sadf/StringRepeat.java
@@ -0,0 +1,10 @@
+package com.csi.sadf;
+
+public class StringRepeat {
+public static void main(String[] args) {
+	String s1="abhi";
+	String string="rajesh";
+	System.out.println(s1==string);
+	System.out.println(s1.equals(string));
+}
+}
